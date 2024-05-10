373d2b071a293dc238142ae664dd3027ea62c723
icarus012832 <icarus012832@gmail.com>
2024-05-10 14:07:53 +0900

    feat: domain 추가 및 다른 클래스들 기초 어노테이션 추가

---
+++ src/main/java/com/mfc/settlement/application/SettlementServiceImpl.java
@@ -0,0 +1,10 @@
+package com.mfc.settlement.application;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SettlementServiceImpl implements SettlementService {
+}
